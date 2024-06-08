571630b6d69a0f16ce6261651b64c357ce1425da
Debabrata-bhakat <bhakat.debabrata@gmail.com>
2024-06-08 12:25:58 +0530

    First Commit

---
+++ magic_script.py
@@ -0,0 +1,29 @@
+import json
+import requests
+
+def return_summary(comp_info, prod_info, tar_group):
+    try: 
+        headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiN2MwNWZjYTEtMjBjYi00YTMxLWExOTktZmNiNWU0ZWE4NzUxIiwidHlwZSI6ImFwaV90b2tlbiJ9.--PS8uDyLyFqxPmT5UNbwmX252WHWILy7yh7sq6WNyo"}
+
+        url = "https://api.edenai.run/v2/text/chat"
+        payload = {
+            "providers": "openai",
+            "text": f"Given the following information about a company and it's product : \nCompany Info : {comp_info} \n Product Info: {prod_info}\n Target Group profile: {tar_group}\n Generate content for video advertising of what our brand ambassador would say about it. You just need to have a monologue of what the person has to say and no actions or anything else. Have it shorter than 20 words.",
+            "chatbot_global_action": "I want to help to generate content for an advertising video.",
+            "previous_history": [],
+            "temperature": 0.0,
+            "max_tokens": 150,
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        result = json.loads(response.text)
+        return(result['openai']['generated_text'])
+    except Exception as e:
+        print(f"An error occured while generating script : {e}")
+        return f"{comp_info},  {prod_info}, {tar_group}"
+
+# comp_info = "Apple"
+# prod_info = "iPhone 14 which is very fast and smooth"
+# tar_group = "young generation"
+# print(return_summary(comp_info,prod_info,tar_group))
---
+++ main.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import time
+import requests
+from magic_script import return_summary
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AUTH = os.getenv('SYNTHESIA_API')
+url = "https://api.synthesia.io/v2/videos"
+
+payload = {
+    "test": "false",
+    "visibility": "public",
+    "input": [
+        {
+            "avatarSettings": {
+                "horizontalAlign": "center",
+                "scale": 1,
+                "style": "rectangular",
+                "seamless": False
+            },
+            "backgroundSettings": {"videoSettings": {
+                "shortBackgroundContentMatchMode": "freeze",
+                "longBackgroundContentMatchMode": "trim"
+            }},
+            "avatar": "anna_costume1_cameraA",
+            "background": "\"off_white\""
+        }
+    ]
+}
+
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "Authorization": AUTH
+}
+
+def main():
+    st.title("Generate commercials for your target consumers in a click! ⚡⚡⚡")
+    st.subheader("Our models churn text to magic 🎩🪄")
+
+    submit_button_clicked = False
+
+    # Create a form to get user input
+    with st.form("my_form"):
+        company = st.text_input("Enter company Information")
+        product = st.text_input("Enter product Information")
+        consumer = st.text_input("Enter target Consumer Profile")
+        title = st.text_input("Enter title for the commercial")
+        # scriptText = "  \n" +company + "  \n" + product + "  \n" + consumer
+        scriptText = return_summary(company,product,consumer)
+        st.write(f"Title: *{title}*")
+        st.write(f"Script: {scriptText}")
+        submit_button = st.form_submit_button("Submit")
+
+
+    if submit_button:
+        payload["title"] = title
+        payload["input"][0]['scriptText'] = scriptText
+        print(scriptText)
+        response = requests.post(url, json=payload, headers=headers).json()
+        print(response)
+        if response.get('status'):
+            submit_button_clicked = True
+            video_id = response.get('id')
+        else:
+            st.error("Some unknown error occured. Please try again after a few minutes")
+
+    # If submit button is clicked, display form details and progress bar
+    if submit_button_clicked:
+        task_completed = False
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.write(f"Title: **{title}")
+            st.write(f"Script: {scriptText}")
+
+        with col2:
+            progress_bar = st.progress(0)
+            status_text = st.empty()
+            max_time = 200
+            hook_url = url + "/" + video_id
+            # hook_url = url  + "/" + '18d24a08-a741-496e-a374-ec848550648c'
+            for i in range(max_time):
+                response = requests.get(hook_url, headers=headers).json()
+                print(i)
+                if response.get('status') == "complete":
+                    data = response['download']
+                    st.video(data, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False,
+                             autoplay=False, muted=False)
+
+                    task_completed = True
+                    break
+                time.sleep(3)  # Simulate some computation
+                current_progress = 100*i//max_time
+                progress_bar.progress(current_progress)
+                status_text.text(f"Progress: {current_progress}%")
+            if task_completed:
+                current_progress = 100
+                progress_bar.progress(current_progress)
+                status_text.text("Task completed! You can now download this video.")
+            else:
+                status_text.text("Our servers are going through a high load. Please ensure that your commercial contains at most 50 words!")
+
+
+if __name__ == '__main__':
+    main()
